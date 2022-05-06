4fa67bd2549429ed1794f40a9dfdb4fe77a9467c
Joseph Villajin <resetdesigns@gmail.com>
2022-05-06 11:28:19 -0700

    Testing Emit Events

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,1 +1,11 @@
-io();
+const socket = io();
+
+socket.on('countUpdated', (count) => {
+	console.log('The count has been updated to ', count);
+});
+
+const button = document.querySelector('#increment');
+button.addEventListener('click', () => {
+	console.log('clicked');
+	socket.emit('increment');
+});
