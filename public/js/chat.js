a56f09c076f6af5df335efd917e27986adcec57d
Joseph Villajin <resetdesigns@gmail.com>
2022-05-10 12:07:10 -0700

    Updated styles for chat

--- public/js/chat.js
+++ public/js/chat.js
@@ -25,7 +25,7 @@ socket.on('locationMessage', (message) => {
 	console.log(message);
 
 	const html = Mustache.render(locationMessageTemplate, {
-		message: message.url,
+		url: message.url,
 		createdAt: moment(message.createdAt).format('h:mm a'),
 	});
 	$messages.insertAdjacentHTML('beforeend', html);
