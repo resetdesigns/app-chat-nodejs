29c2cf061e7a5e6afecb15c04bc3a9bdca92ab9c
Joseph Villajin <resetdesigns@gmail.com>
2022-06-02 17:11:15 -0700

    Storing Users and Enabling Chat

--- public/js/chat.js
+++ public/js/chat.js
@@ -10,6 +10,7 @@ const $messages = document.querySelector('#messages');
 // Templates
 const messageTemplate = document.querySelector('#message-template').innerHTML;
 const locationMessageTemplate = document.querySelector('#location-message-template').innerHTML;
+const sidebarTemplate = document.querySelector('#sidebar-template').innerHTML;
 
 // Options
 const { username, room } = Qs.parse(location.search, { ignoreQueryPrefix: true });
@@ -18,6 +19,7 @@ socket.on('message', (message) => {
 	console.log(message);
 
 	const html = Mustache.render(messageTemplate, {
+		username: message.username,
 		message: message.text,
 		createdAt: moment(message.createdAt).format('h:mm a'),
 	});
@@ -28,10 +30,19 @@ socket.on('locationMessage', (message) => {
 	console.log(message);
 
 	const html = Mustache.render(locationMessageTemplate, {
+		username: message.username,
 		url: message.url,
 		createdAt: moment(message.createdAt).format('h:mm a'),
 	});
 	$messages.insertAdjacentHTML('beforeend', html);
+});
+
+socket.on('roomData', ({ room, users }) => {
+	const html = Mustache.render(sidebarTemplate, {
+		room,
+		users,
+	});
+	document.querySelector('#sidebar').innerHTML = html;
 });
 
 $messageForm.addEventListener('submit', (e) => {
@@ -74,7 +85,16 @@ $sendLocationButton.addEventListener('click', () => {
 	});
 });
 
-socket.emit('join', {
-	username,
-	room,
-});
+socket.emit(
+	'join',
+	{
+		username,
+		room,
+	},
+	(error) => {
+		if (error) {
+			alert(error);
+			location.href = '/';
+		}
+	}
+);
